6f05bf7490367f88a7544681f6f26afba42417ff
carlgrefiel <123727084+carlgrefiel@users.noreply.github.com>
2023-03-08 22:55:44 +0800

    added validation using react hook form

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 <img className=" shadow-2xl mx-auto w-[400px] h-[400px] rounded-lg" alt="hnu"  src={hnu}/>
             </div>
             <div className=' lg:w-1/2 mx-auto shadow-2xl p-5 rounded-lg'>
-                <p className="lg:text-xl md:text-lg text-md py-8 leading-8 px-16  text-center">
+                <p className="lg:text-xl md:text-lg text-md py-8 leading-8 lg:px-16 px-1 text-center">
                     I earned a Bachelor of Science in Information Technology from Holy Name University. I learned much about programming languages like HTML, CSS, JavaScript, and PHP at Holy Name University. I also improved my design abilities by learning how to create visually appealing and user-friendly user interfaces. I finished several projects that required me to collaborate with other students to develop websites and applications that met specific requirements and produced measurable results  
                 </p>
             </div>
